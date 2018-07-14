1a0e33f6a88b4e45d59dc2ee46ce185bbdaa9e64
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2018-07-14 07:04:30 +0300

    fix UIManagerStubModule: UIManager is undefined. The native module config is probably incorrect.

--- android/src/main/java/com/reactlibrary/UIManagerStubModule.java
+++ android/src/main/java/com/reactlibrary/UIManagerStubModule.java
@@ -2,6 +2,7 @@ package com.reactlibrary;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
 public class UIManagerStubModule extends ReactContextBaseJavaModule {
 
@@ -12,5 +13,11 @@ public class UIManagerStubModule extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return "UIManager";
+    }
+
+    //module will not be loaded correctly without at least one ReactMethod defined
+    @ReactMethod
+    public void foo() {
+
     }
 }
